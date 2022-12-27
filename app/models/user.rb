4c19d65a2e09dad1893d32078288dedfc2a7f2e8
Jared Rader <jaredrader@Jareds-MacBook-Pro-2.local>
2022-12-27 15:23:53 -0600

    build the authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
-
-  before_validation :strip_extraneous_spaces
+  has_many :app_sessions
 
   has_secure_password
+
+  before_validation :strip_extraneous_spaces
 
   validates :name, presence: true
   validates :password, presence: true, length: { minimum: 8 }
@@ -16,6 +17,18 @@ class User < ApplicationRecord
             uniqueness: {
               case_sensitive: false
             }
+
+  def self.create_app_session(email:, password:)
+    return nil unless user = User.find_by(email: email.downcase)
+
+    user.app_sessions.create if user.authenticate(password)
+  end
+
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
 
   private
 
