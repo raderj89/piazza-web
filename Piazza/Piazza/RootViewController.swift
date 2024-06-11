ef2f7473adced40f7566b1e7c2ca75c7be01c859
raderj89 <raderj89@gmail.com>
2024-06-11 15:49:03 -0500

    Update to new version

--- Piazza/Piazza/RootViewController.swift
+++ Piazza/Piazza/RootViewController.swift
@@ -8,7 +8,17 @@
 import UIKit
 
 class RootViewController: UITabBarController {
-
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        let tabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithOpaqueBackground()
+        UITabBar.appearance().scrollEdgeAppearance = tabBarAppearance
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUp();
